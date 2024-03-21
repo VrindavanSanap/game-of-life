43a9335ddfea79b099af849101a3f704e3e1e5fa
Vrindavan Sanap <vrindavansanap@gmail.com>
2024-03-21 08:57:18 +0530

    -

--- js/main.js
+++ js/main.js
@@ -1,12 +1,46 @@
+
+function make2darray(cols, rows) {
+    let arr = new Array(cols)
+    for (let i = 0; i < cols; i++) {
+        arr[i] = new Array(rows)
+    }
+    return arr
+}
+
+let grid;
+let cols;
+let rows;
+let resolution = 40
+
 function setup() {
-    createCanvas(windowWidth, windowHeight);
+    // createCanvas(windowWidth, windowHeight);
+    createCanvas(400, 400);
+    cols = width / resolution;
+    rows = height / resolution;
+    grid = make2darray(cols, rows)
+    for (let i = 0; i < cols; i++) {
+        for (let j = 0; j < rows; j++) {
+            grid[i][j] = floor(random(2))
+        }
+    }
 }
 
 function draw() {
-    background(255);
-    text("put your p5.js code here",10, frameCount % height);
+    background(0)
+    let next = make2darray(cols, rows)
+
+    for (let i = 0; i < cols; i++) {
+        for (let j = 0; j < rows; j++) {
+            let x = i * resolution
+            let y = j * resolution
+            if (grid[i][j]) {
+                rect(x, y, resolution, resolution)
+            }
+        }
+    }
+
 }
 
 function windowResized() {
-    resizeCanvas(windowWidth, windowHeight);
+    // resizeCanvas(windowWidth, windowHeight);
 }
